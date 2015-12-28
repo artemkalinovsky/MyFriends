6c6da29e9a9e10757efec66eed43bb927d9c5f0f
Artem Kalinovskyi <artem.kalinovsky@gmail.com>
2015-12-28 22:25:22 +0200

    Initial Commit

---
+++ MyFriends/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  MyFriends
+//
+//  Created by Artem Kalinovsky on 28/12/2015.
+//  Copyright © 2015 Artem Kalinovsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ MyFriends/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyFriends
+//
+//  Created by Artem Kalinovsky on 28/12/2015.
+//  Copyright © 2015 Artem Kalinovsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
